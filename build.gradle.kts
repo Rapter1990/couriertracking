92aa1543652ab3d1234363f29ee65b356751412e
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-07-26 14:40:15 +0300

    Task 4 : Revise build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.6"
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
+	kotlin("plugin.jpa") version "1.9.24"
 }
 
 group = "com.casestudy"
