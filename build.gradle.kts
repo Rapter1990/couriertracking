6280f0b68b3e990b8d81b7022ed2358c7debde8d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-07-26 10:34:51 +0300

    Task 1 : Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
+	kotlin("jvm") version "1.9.24"
+	kotlin("plugin.spring") version "1.9.24"
+}
+
+group = "com.casestudy"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(21))
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web") // Added this line
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	jvmToolchain {
+		languageVersion.set(JavaLanguageVersion.of(21))
+	}
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/casestudy/couriertracking/CouriertrackingApplication.kt
@@ -0,0 +1,11 @@
+package com.casestudy.couriertracking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouriertrackingApplication
+
+fun main(args: Array<String>) {
+	runApplication<CouriertrackingApplication>(*args)
+}
