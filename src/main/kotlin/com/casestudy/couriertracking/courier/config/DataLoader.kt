106a7ab79a5a9ac9de169232e944a4d6a10b0513
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-07-29 08:36:19 +0300

    Task 11 : Revise approach to initialize data in DataLoader

--- src/main/kotlin/com/casestudy/couriertracking/courier/config/DataLoader.kt
+++ src/main/kotlin/com/casestudy/couriertracking/courier/config/DataLoader.kt
@@ -2,25 +2,45 @@ package com.casestudy.couriertracking.courier.config
 
 import com.casestudy.couriertracking.courier.model.entity.StoreEntity
 import com.casestudy.couriertracking.courier.repository.StoreRepository
-import com.fasterxml.jackson.core.type.TypeReference
+import lombok.RequiredArgsConstructor
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import lombok.RequiredArgsConstructor
-import org.springframework.core.io.ResourceLoader
 
 @Configuration
 @RequiredArgsConstructor
-class DataLoader(private val resourceLoader: ResourceLoader, private val storeRepository: StoreRepository) {
+class DataLoader(private val storeRepository: StoreRepository) {
 
     @Bean
     fun loadInitialData(): CommandLineRunner {
         return CommandLineRunner {
-            val objectMapper = jacksonObjectMapper()
-            val resource = resourceLoader.getResource("classpath:stores.json")
-            val file = resource.file
-            val stores: List<StoreEntity> = objectMapper.readValue(file, object : TypeReference<List<StoreEntity>>() {})
+            val stores = listOf(
+                    StoreEntity(
+                            name = "Market 1",
+                            lat = 40.9923307,
+                            lng = 29.1244229
+                    ),
+                    StoreEntity(
+                            name = "Market 2",
+                            lat = 40.986106,
+                            lng = 29.1161293
+                    ),
+                    StoreEntity(
+                            name = "Market 3",
+                            lat = 41.0066851,
+                            lng = 28.6552262
+                    ),
+                    StoreEntity(
+                            name = "Market 4",
+                            lat = 41.055783,
+                            lng = 29.0210292
+                    ),
+                    StoreEntity(
+                            name = "Market 5",
+                            lat = 40.9632463,
+                            lng = 29.0630908
+                    )
+            )
             storeRepository.saveAll(stores)
         }
     }
