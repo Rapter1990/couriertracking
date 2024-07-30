6f5896a9e3dd7560edf3a89fc560058b0cc84f87
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2024-07-30 08:18:39 +0300

    Task 40 : Revise CourierControllerTest

--- src/test/kotlin/com/casestudy/couriertracking/courier/controller/CourierControllerTest.kt
+++ src/test/kotlin/com/casestudy/couriertracking/courier/controller/CourierControllerTest.kt
@@ -7,6 +7,9 @@ import com.casestudy.couriertracking.courier.model.dto.request.TravelQueryReques
 import com.casestudy.couriertracking.courier.model.mapper.CourierToCourierResponseMapper
 import com.casestudy.couriertracking.courier.service.CourierService
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doNothing
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -30,6 +33,8 @@ class CourierControllerTest : AbstractRestControllerTest() {
      */
     @Test
     fun `logCourierLocation should return success message`() {
+
+        // Given
         val logRequest = LogCourierLocationRequest(
                 courierId = "123e4567-e89b-12d3-a456-426614174000",
                 lat = 37.7749,
@@ -37,11 +42,20 @@ class CourierControllerTest : AbstractRestControllerTest() {
                 timestamp = LocalDateTime.now()
         )
 
+        // When
+        doNothing().`when`(courierService).logCourierLocation(any())
+
+
+        // Then
         mockMvc.perform(MockMvcRequestBuilders.post("/api/couriers/log-location")
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(logRequest)))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response").value("Location logged successfully."))
+
+        // Verify
+        verify(courierService).logCourierLocation(any())
+
     }
 
     /**
@@ -49,6 +63,8 @@ class CourierControllerTest : AbstractRestControllerTest() {
      */
     @Test
     fun `getPastTravels should return a list of travels`() {
+
+        // Given
         val courierId = "123e4567-e89b-12d3-a456-426614174000"
         val travels = listOf(
                 Courier(id = "1", courierId = courierId, lat = 37.7749, lng = -122.4194, storeName = "store1", timestamp = LocalDateTime.now()),
@@ -58,13 +74,18 @@ class CourierControllerTest : AbstractRestControllerTest() {
 
         val response = courierToCourierResponseMapper.map(travels)
 
+        // When
         whenever(courierService.getPastTravelsByCourierId(courierId)).thenReturn(travels)
 
+        // Then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/couriers/travels/{courierId}", courierId))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response").isArray)
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response[0].storeName").value(response.get(0).storeName))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response[1].storeName").value(response.get(1).storeName))
+
+        // Verify
+        verify(courierService).getPastTravelsByCourierId(courierId)
 
     }
 
@@ -73,6 +94,8 @@ class CourierControllerTest : AbstractRestControllerTest() {
      */
     @Test
     fun `getTravelsByCourierIdStoreNameAndTimeRange should return filtered travels`() {
+
+        // Given
         val request = TravelQueryRequest(
                 courierId = "123e4567-e89b-12d3-a456-426614174000",
                 storeName = "Ataşehir MMM Migros",
@@ -93,14 +116,19 @@ class CourierControllerTest : AbstractRestControllerTest() {
 
         val response = courierToCourierResponseMapper.map(travels)
 
+        // When
         whenever(courierService.getTravelsByCourierIdStoreNameAndTimeRange(request)).thenReturn(travels)
 
+        // Then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/couriers/travels")
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(request)))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response").isArray)
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response[0].storeName").value(response[0].storeName))
+
+        // Verify
+        verify(courierService).getTravelsByCourierIdStoreNameAndTimeRange(request)
 
     }
 
@@ -109,15 +137,24 @@ class CourierControllerTest : AbstractRestControllerTest() {
      */
     @Test
     fun `getTotalTravelDistance should return total distance traveled by a courier`() {
+
+        // Given
         val courierId = "123e4567-e89b-12d3-a456-426614174000"
         val totalDistance = 0.097 // Example distance in kilometers
         val formattedDistance = String.format("%.2f km", totalDistance)
+
+        // When
         whenever(courierService.getTotalTravelDistance(courierId)).thenReturn(totalDistance)
 
+        // Then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/couriers/travels/{courierId}/total-distance", courierId)
                 .contentType("application/json"))
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andExpect(MockMvcResultMatchers.jsonPath("$.response").value(formattedDistance))
+
+        // Verify
+        verify(courierService).getTotalTravelDistance(courierId)
+
     }
 
 }
